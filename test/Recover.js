83e76b17f5f9fd516202e1c548efeb866c69eec7
skyap <albert_einstein_1879_3_14@hotmail.com>
2023-01-16 08:52:03 +0800

    fist commit

--- test/Recover.js
+++ test/Recover.js
@@ -22,24 +22,26 @@ describe("Test signing",async()=>{
 
     it("sign 2",async()=>{
         const [signer] = await ethers.getSigners();
-        console.log(signer.address);
+        // console.log(signer.address);
         const abiCoder = new ethers.utils.AbiCoder();
         const encoded = ethers.utils.keccak256(abiCoder.encode(["string","uint256"], ["yap",40]));
         const signature = await signer.signMessage(ethers.utils.arrayify(encoded));
-        console.log(signature);
-        console.log(await contract.recover2(encoded,signature));
+        // console.log(signature);
+        // console.log(await contract.recover2(encoded,signature));
+        expect(await contract.recover2(encoded,signature)).to.equal(signer.address);
     })
 
 
     it("sign 3",async()=>{
         const [signer] = await ethers.getSigners();
-        console.log(signer.address);
+        // console.log(signer.address);
         const abiCoder = new ethers.utils.AbiCoder();
         // const encoded = ethers.utils.keccak256(abiCoder.encode(["string","uint256"], ["yap",40]));
         const encoded = ethers.utils.keccak256(ethers.utils.solidityPack(["string","uint256"], ["yap",40]));
         const signature = await signer.signMessage(ethers.utils.arrayify(encoded));
-        console.log(signature);
-        console.log(await contract.recover3({name:"yap",age:40},signature));
+        // console.log(signature);
+        // console.log(await contract.recover3({name:"yap",age:40},signature));
+        expect(await contract.recover3({name:"yap",age:40},signature)).to.equal(signer.address);
     })
 
 
